a5aef52eeb584854e6605378c03dda02608a01ea
異次元のA <165989579+450395Ayumu@users.noreply.github.com>
2025-10-28 11:39:03 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,12 +1,12 @@
 class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/450395Ayumu/homebrew-dote"
-  url "https://raw.githubusercontent.com/450395Ayumu/homebrew-dote/main/dote.c"
+  url "https://raw.githubusercontent.com/450395Ayumu/homebrew-dote/main/dot9.c"
   sha256 "a0a56901a21cce7d9305d9e6f301726829d88393bd5dd4437c241207414f73d8"
   version "1.0.0"
 
   def install
-    system ENV.cc, "dote.c", "-o", "dote"
+    system ENV.cc, "dot9.c", "-o", "dote"
     bin.install "dote"
   end
 end
